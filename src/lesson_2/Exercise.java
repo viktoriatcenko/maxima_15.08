f5fc77511c0633fb0c6e841568c264f00ee4ad44
viktoriatsenko <nashtrenerishka@gmail.com>
2024-08-19 21:09:27 +0300

    Initial commit

---
+++ src/lesson_2/Exercise.java
@@ -0,0 +1,21 @@
+package lesson_2;
+
+public class Exercise {
+    public static void main(String[] args) {
+        // Задача 1. Дано: a = 60, b = 45, c = 17. Найти периметр
+
+        int a = 60;
+        int b = 45;
+        int c = 17;
+        int p = a + b + c;
+
+        System.out.println(p);
+
+        // Задача 3. Дано: Коля получил много двоек и закопал свой дневник на 127 метров.
+        // Потом вернулся и углубился еще на 2 метра. На сколько метров закопал свой дневник Коля?
+        byte a1 = 127;
+        byte c1 = (byte) (a1 + 2);
+
+        System.out.println(c1);
+    }
+}
---
+++ src/lesson_2/Main.java
@@ -0,0 +1,59 @@
+package lesson_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Something
+
+        // Переменная
+
+        // 1 - Обозначить тип данных, переменную которых мы хотим объявить
+        // 2 - Дать этой переменной имя
+        // 3 - Через вызов оператора присвоения (=) присвоить переменной значение / ПРОИНИЦИАЛИЗИРОВАТЬ
+        byte a;
+        a = 123;
+        System.out.println(a);
+        a = 50;
+        System.out.println(a);
+        short s = 12312;
+        System.out.println(s);
+        s = 6548;
+        System.out.println(s);
+        int i = 1236548;
+        System.out.println(i);
+        i = 6549878;
+        System.out.println(i);
+        long l = 123565668762L;
+        System.out.println(l);
+        l = 5774623486123L;
+        System.out.println(l);
+        float f = 10.5555555555f;
+        System.out.println(f);
+        double d = 1.6666666777777;
+        System.out.println(d);
+
+        char myFirstChar = '!';
+        char mySecondChar = 43;
+
+        System.out.println(myFirstChar);
+        System.out.println(mySecondChar);
+
+        boolean b = true;
+        System.out.println(b);
+
+        boolean math = f > d;
+
+        System.out.println(math);
+
+
+        int first = 25;
+
+        int second = 50;
+
+        int third = first * 10;
+
+        System.out.println(third);
+
+
+    }
+}
